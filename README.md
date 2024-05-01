451155918b07c068774e600a531be978b487317f
Clifford Mbithuka <cliffordmusyoka1@gmail.com>
2024-05-01 16:42:22 +0300

    Add synchronous features

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 ## ERROR HANDLING
 
+## TYPES OF ERRORS
+
+1. referenceError - Occurs when trying to access a variable that does not exist. Also when referencing an undeclared variable or calling a function that is not defined
+
+2. Syntax Errors: These occur when the code violates the syntax rules of JavaScript. For example, missing semicolons, unmatched parentheses, or misspelled keywords can lead to syntax errors.
+  (caught when we are writing our code)
+
+3. Type Errors: These occur when an operation is performed on a value of the wrong type.
+
+
 Errors are encountered due to ( Network errors, Promise Rejection, security errors )
+
+The syntax construct try...catch allows us to catch errors for the script to do something more reasonable instead of dying.
+
 1. try { } - encloses code that might potentially cause an error
 2. catch { } - Catch and handle any thrown errors from try { }
-- catch uses one parameter (err) and inside the catch use console.error(err)
+- catch uses one parameter (err) 
 4. finally { } - (optional) always executes used mostly for clean up Eg. close files, close connections, release resources.( Whether or not an error occurs)
+
+* try...catch only works with runtime errors.
+* try...catch works synchronously
--- index.js
+++ index.js
@@ -1,18 +1,51 @@
+"use strict"
+//  Engineer = "cliff"; // ReferenceError: Engineer is not defined
+// console.log(papa);
+
+
+// const wecode = [Nelly, Max, James, cliff]; //reference error: names not defined
+
+// console.log(wecode; // syntax error: missing closing bracket 
+
+
+// const name = "JohnBrian";
+// name = "Hope"; //Type Error: Assignment to a constant variable 
+// console.log(name);
+
+
+//try...catch syntax 
+// const makeError = () => {
+//   try{
+//     const name = "JohnBrian";
+//     name = "Hope";
+//   }catch(e){
+// console.error(e); 
+//   } finally {
+//     console.log("....Successfully Submitted"); //executed irregardless of an error or not
+//   }
+// }
+// makeError();
+
+              //Asynchronous
 // setTimeout(function() {
 //     try {
-//       wecode; // try...catch handles the error!
-//     } catch {
-//       alert( "error is caught here!" );
+//       wecode; // Error is not defined
+//     } catch(err) {
+//       console.error(err);
 //     }
 //   }, 1000);
 
-  let json = '{ "age": 30 }'; // incomplete data
+
+  function divide(x, y) {
+    if (y === 0) {
+        throw new Error("Division by zero is not allowed");
+    }
+    return x / y;
+}
 
 try {
-
-  let user = JSON.parse(json); // <-- no errors
-  alert( user.name ); // no name!
-
-} catch (err) {
-  alert( "doesn't execute" );
+    const result = divide(10, 0);
+    console.log("Result:", `${result}`); // This line will not be executed
+} catch (error) {
+    console.error("An error occurred:", `${error}`);
 }
